7a0bff2a4994ad88111e4db30600acf9e33d86f8
Christian Villamin <ChristianVillamin31@gmail.com>
2022-11-19 14:01:41 +0800

    feat: card and instructionText component

---
+++ components/game/InstructionText.tsx
@@ -0,0 +1,22 @@
+import { COLORS } from '@utils/constants';
+import { StyleSheet, Text } from 'react-native';
+
+type Props = {
+  children: string;
+  style?: Record<string, {}>;
+};
+
+InstructionText.defaultProps = {
+  style: {},
+};
+
+export default function InstructionText({ children, style }: Props) {
+  return <Text style={[styles.instruction, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  instruction: {
+    color: COLORS.accent500,
+    fontSize: 24,
+  },
+});
---
+++ components/ui/Card.tsx
@@ -0,0 +1,27 @@
+import { COLORS } from '@utils/constants';
+import { StyleSheet, View } from 'react-native';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Card({ children }: Props) {
+  return <View style={style.container}>{children}</View>;
+}
+
+const style = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.primary800,
+    marginTop: 36,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    elevation: 4, // Android Shadow
+    shadowColor: 'black', // iOS Shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
