584ecf0b421144e11c98af2e229df4e132dc1c08
Christian Villamin <ChristianVillamin31@gmail.com>
2022-11-19 14:19:23 +0800

    feat: app splash while loading resource

--- App.tsx
+++ App.tsx
@@ -2,13 +2,47 @@ import GameOverScreen from '@screens/GameOverScreen';
 import GameScreen from '@screens/GameScreen';
 import StartGameScreen from '@screens/StartGameScreen';
 import { COLORS } from '@utils/constants';
+import * as Font from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
 import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [userNumber, setUserNumber] = useState<number | null>(null);
   const [isGameOver, setIsGameOver] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          'open-sans': require('@assets/fonts/OpenSans-Regular.ttf'),
+          'open-sans-bold': require('@assets/fonts/OpenSans-Bold.ttf'),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   function handlePickedNumber(pickedNumber: number) {
     setUserNumber(pickedNumber);
@@ -33,6 +67,7 @@ export default function App() {
     <LinearGradient
       colors={[COLORS.primary700, COLORS.accent500]}
       style={styles.container}
+      onLayout={onLayoutRootView}
     >
       <ImageBackground
         source={require('./assets/images/background.png')}
