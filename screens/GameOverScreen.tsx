cfb50ce7a92c1c56911b2fe38e98d6eb0813d4d4
Christian Villamin <ChristianVillamin31@gmail.com>
2022-11-19 15:41:41 +0800

    feat: gameover screen styling

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,5 +1,45 @@
-import { Text } from 'react-native';
+import Title from '@components/ui/Title';
+import COLORS from '@utils/constants';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Game is over!</Title>
+
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('@assets/images/success.png')}
+          resizeMode="cover"
+        />
+      </View>
+
+      <View>
+        <Text>Your phone needed X rounds to guess the number Y.</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: COLORS.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
